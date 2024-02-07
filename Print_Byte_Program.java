d1449de5ab90266817bfc288577de9b9b293fc67
Yash Bhande <138775827+yashbhande@users.noreply.github.com>
2024-02-07 19:32:29 +0530

    Create Print_Byte_Program.java

---
+++ Print_Byte_Program.java
@@ -0,0 +1,18 @@
+// Class declaration for the Print_Byte_Program class.
+class Print_Byte_Program {
+
+    // Main method declaration.
+    public static void main(String[] pbp) {
+
+        // Variable declaration and initialization with a byte value.
+        byte variable1 = 20;
+
+        // Printing the value of the byte variable.
+        System.out.println(variable1);
+    }
+}
+
+/* Expected Output:
+   Compile time: Success
+   Run Time: Success
+*/
