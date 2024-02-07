16ce7033e75b06968eb72c355455c20801d877e9
Yash Bhande <138775827+yashbhande@users.noreply.github.com>
2024-02-07 19:44:10 +0530

    Create Print_Short_Program.java

---
+++ Print_Short_Program.java
@@ -0,0 +1,13 @@
+// This class demonstrates printing a short variable.
+class Print_Short_Program {
+  
+    // Main method where execution begins.
+    public static void main(String[] PSP) {
+      
+        // Variable declaration and initialization with a short value.
+        short this_is_byte_variable = 200;
+
+        // Printing the value of the short variable.
+        System.out.println(this_is_byte_variable);
+    }
+}
