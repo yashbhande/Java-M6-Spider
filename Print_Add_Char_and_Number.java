5f7bf2c6bb4bdff1dd3a14bedafceb51027fd6f8
Yash Bhande <138775827+yashbhande@users.noreply.github.com>
2024-02-08 23:03:38 +0530

    Add files via upload

---
+++ Print_Add_Char_and_Number.java
@@ -0,0 +1,18 @@
+class Print_Add_Char_and_Number
+{
+public static void main(String[] PASN)
+{
+char var = 'a';
+int var2=10;
+
+System.out.println(var+var2);
+}
+}
+
+
+/*Output
+
+CT: Sucess
+RT: Failed
+
+*/
