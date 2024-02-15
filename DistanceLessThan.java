379403a7bd4a7811915caf61083feee09f50151f
Yash Bhande <138775827+yashbhande@users.noreply.github.com>
2024-02-15 13:11:43 +0530

    Create DistanceLessThan.java

---
+++ DistanceLessThan.java
@@ -0,0 +1,13 @@
+class DistanceLessThan{
+public static void main(String[] args){
+
+int distance=15;
+int distance1=15;
+
+boolean result=distance<distance1;
+
+System.out.println("Distance is less than distance1 "+ result);
+//false
+
+}
+}
