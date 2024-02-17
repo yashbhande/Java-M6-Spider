1b5a09d8b3941c1e463777b3ba2dfe11f90004a8
Yash Bhande <138775827+yashbhande@users.noreply.github.com>
2024-02-17 16:37:11 +0530

    Create Ternary_Two_Operands.java

---
+++ Ternary_Two_Operands.java
@@ -0,0 +1,17 @@
+// Define a class named Ternary_Two_Operands
+class Ternary_Two_Operands {
+    // Main method
+    public static void main(String[] args) {
+        
+        // Declare and initialize two integer variables
+        int a = 10, b = 20;
+        
+        // Using ternary operator to determine which of the two operands is greatest
+        // If a is greater than b, assign "A is Greatest" to c, otherwise assign "B is Greatest" to c
+       
+      String c = (a > b) ? ("A is Greatest") : ("B is Greatest");
+        
+        // Print the result, which will be "B is Greatest" as b is greater than a
+        System.out.println(c);
+    }
+}
