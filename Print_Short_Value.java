6a61906a8d5ae00212f810d31ec0c04d8beb5332
Yash Bhande <138775827+yashbhande@users.noreply.github.com>
2024-02-17 19:21:50 +0530

    Update and rename Print_Short_Program.java to Print_Short_Value.java

---
+++ Print_Short_Value.java
@@ -0,0 +1,17 @@
+// Define a class named Print_Short_Value
+class Print_Short_Value {
+
+    // Main method declaration.
+    public static void main(String[] PSP) {
+
+        // Variable declaration and initialization with a short value.
+        short this_is_byte_variable = 200;
+
+        // Printing the value of the short variable.
+        System.out.println(this_is_byte_variable); // Expected output: 200
+
+        // Short range: -32768 to 32767
+        // The value 200 falls within the range of the short data type.
+
+    }
+}
