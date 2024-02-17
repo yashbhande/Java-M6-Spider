cefa0022d981452c73543f11c71fbd8b06bac496
Yash Bhande <138775827+yashbhande@users.noreply.github.com>
2024-02-17 19:19:44 +0530

    Update and rename Print_Long_Program.java to Print_Long_Value.java

---
+++ Print_Long_Value.java
@@ -0,0 +1,21 @@
+// Define a class named Print_Long_Value
+class Print_Long_Value {
+
+    // Main method declaration.
+    public static void main(String[] plp) {
+
+        // Variable declaration and initialization with a long value.
+        long variable1 = 2000000;
+
+        // Printing the value of the long variable.
+        System.out.println(variable1); // Expected output: 2000000
+
+        // Both compile time and run time are expected to be successful.
+
+    }
+}
+
+/* Expected Output:
+   Compile time: Success
+   Run Time: Success
+*/
