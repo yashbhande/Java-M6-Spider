9bb088a2a61e32768f2247df16711b1cae23c1fd
Yash Bhande <138775827+yashbhande@users.noreply.github.com>
2024-02-17 19:18:38 +0530

    Update and rename Print_Int_Program.java to Print_Int_Value.java

---
+++ Print_Int_Value.java
@@ -0,0 +1,21 @@
+// Define a class named Print_Int_Value
+class Print_Int_Value {
+
+    // Main method declaration.
+    public static void main(String[] pbp) {
+
+        // Variable declaration and initialization with an int value.
+        int variable1 = 20;
+
+        // Printing the value of the int variable.
+        System.out.println(variable1); // Expected output: 20
+
+        // Both compile time and run time are expected to be successful.
+
+    }
+}
+
+/* Expected Output:
+   Compile time: Success
+   Run Time: Success
+*/
