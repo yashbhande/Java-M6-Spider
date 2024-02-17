10e21238b9cdf81e1fd0617f82523d6221b5713a
Yash Bhande <138775827+yashbhande@users.noreply.github.com>
2024-02-17 19:17:59 +0530

    Update and rename Print_Float_Program.java to Print_Float_Value.java

---
+++ Print_Float_Value.java
@@ -0,0 +1,21 @@
+// Define a class named Print_Float_Value
+class Print_Float_Value {
+
+    // Main method declaration.
+    public static void main(String[] pfp) {
+
+        // Variable declaration and initialization with a float value.
+        float variable1 = 20.2f;
+
+        // Printing the value of the float variable.
+        System.out.println(variable1); // Expected output: 20.2
+
+        // Both compile time and run time are expected to be successful.
+
+    }
+}
+
+/* Expected Output:
+   Compile time: Success
+   Run Time: Success
+*/
