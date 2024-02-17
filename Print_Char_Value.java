b388b012c3f9eaef2e72271345bc76da8ab6da82
Yash Bhande <138775827+yashbhande@users.noreply.github.com>
2024-02-17 19:16:17 +0530

    Update and rename Print_Char_Program.java to Print_Char_Value.java

---
+++ Print_Char_Value.java
@@ -0,0 +1,21 @@
+// Define a class named Print_Char_Value
+class Print_Char_Value {
+
+    // Main method declaration.
+    public static void main(String[] pcp) {
+
+        // Variable declaration and initialization with a char value.
+        char variable1 = 'B';
+
+        // Printing the value of the char variable.
+        System.out.println(variable1); // Expected output: B
+
+        // Both compile time and run time are expected to be successful.
+
+    }
+}
+
+/* Expected Output:
+   Compile time: Success
+   Run Time: Success
+*/
