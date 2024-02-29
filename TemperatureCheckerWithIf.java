d3421287d213c88366e61a6d2a568c44bb2caa2c
Yash Bhande <138775827+yashbhande@users.noreply.github.com>
2024-02-29 10:05:19 +0530

    Create TemperatureCheckerWithIf.java

---
+++ TemperatureCheckerWithIf.java
@@ -0,0 +1,15 @@
+// This class demonstrates temperature checking using if statements.
+public class TemperatureCheckerWithIf {
+    public static void main(String[] args) {
+        // Temperature value
+        int temperature = 25;
+
+        // Checking if temperature is greater than 30
+        if (temperature > 30) {
+            System.out.println("It's a hot day!");
+        }
+        if (temperature <= 30) {
+            System.out.println("It's a normal day.");
+        }
+    }
+}
