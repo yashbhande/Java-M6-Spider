f21bbcc0cdc67cd049d1a08f0917c603b1afc88a
Yash Bhande <138775827+yashbhande@users.noreply.github.com>
2024-03-10 19:55:23 +0530

    Create MonthName.java

---
+++ MockExamsPrograms/MonthName.java
@@ -0,0 +1,51 @@
+// Online Java Compiler
+// Use this editor to write, compile, and run your Java code online
+
+// Class to print the name of the month based on the given number
+class MonthName {
+    public static void main(String[] args) {
+        int month = 11; // Month number to determine its name
+        
+        // Switch statement to match the month number and print the corresponding month name
+        switch (month) {
+            case 1:
+                System.out.println("January");
+                break;
+            case 2:
+                System.out.println("February");
+                break;
+            case 3:
+                System.out.println("March");
+                break;
+            case 4:
+                System.out.println("April");
+                break;
+            case 5:
+                System.out.println("May");
+                break;
+            case 6:
+                System.out.println("June");
+                break;
+            case 7:
+                System.out.println("July");
+                break;
+            case 8:
+                System.out.println("August");
+                break;
+            case 9:
+                System.out.println("September");
+                break;
+            case 10:
+                System.out.println("October");
+                break;
+            case 11:
+                System.out.println("November");
+                break;
+            case 12:
+                System.out.println("December");
+                break;
+            default:
+                System.out.println("Not a valid month");
+        }
+    }
+}
