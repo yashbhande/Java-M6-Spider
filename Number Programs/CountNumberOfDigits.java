daf61094456be6eb645f4d38ffaeb29e926380a8
Yash Bhande <138775827+yashbhande@users.noreply.github.com>
2024-03-17 15:51:03 +0530

    Create CountNumberOfDigits.java

---
+++ Number Programs/CountNumberOfDigits.java
@@ -0,0 +1,15 @@
+//This program demonstrate how to count number of digits.
+
+class CountNumberOfDigits {
+    public static void main(String[] args) {
+       int num=1234567;
+       int count=0;
+
+       while(num!=0) //repeat loop until num==0
+       {
+           count++; //increase count as loop started
+           num/=10;  //decrease number last digit , 123456
+       }
+       System.out.println("Count of Digits : "+count);
+    }
+}
