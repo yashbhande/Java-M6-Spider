d230d69167652d33e3ca48cc04f9f4fcc852f7d8
Yash Bhande <138775827+yashbhande@users.noreply.github.com>
2024-03-17 22:40:04 +0530

    Update PrimeChecker.java

--- While Loop/PrimeChecker.java
+++ While Loop/PrimeChecker.java
@@ -1,31 +1,31 @@
-
- //Program to check whether a given number is prime or not
+// Prime numbers are natural numbers that are divisible by only 1 and the number itself.
+// Program to check whether a given number is prime or not
 
 public class PrimeChecker {
     public static void main(String[] args) {
  
         int num=13; // Number to check
-        int c=0; // Counter for factors
+        int counter=0; // Counter for factors
         int i=2; // Starting divisor
-        int it=0; // Iterator count
+//        int it=0; // Iterator count
         
         // Loop to check factors of the number
         while(i<=num/2)
         {
             if(num%i==0)
             {
-                c++; // Increment counter if num is divisible by i
+                counter++; // Increment counter if num is divisible by i
         
             }
             i++; // Move to the next divisor
-            it++; // Increment iterator count
+ //           it++; // Increment iterator count
         }
  
         // Print the number of loop iterations
-        System.out.println("Loop Iteration happened: "+it);
+   //     System.out.println("Loop Iteration happened: "+it);
         
         // Check if the number is prime or not and print the result
-        if(c==0){
+        if(counter==0){
             System.out.println("Prime");
         }
         else
