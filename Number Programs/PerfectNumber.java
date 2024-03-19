57bc7029e2d6cef6b21a4437062b1a8a3a7b4c1a
Yash Bhande <138775827+yashbhande@users.noreply.github.com>
2024-03-19 23:28:03 +0530

    Create PerfectNumber.java

---
+++ Number Programs/PerfectNumber.java
@@ -0,0 +1,33 @@
+/*
+!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+
+ Confused with code 
+
+!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+
+class PerfectNumber {
+    public static void main(String[] args) {
+    int num=4;
+    int i=1;
+    int sqr=0;
+    
+    while(sqr<=num)
+    {
+        if(sqr==num)
+        {
+        System.out.println("Number is perfect number");
+        break;
+        }
+        sqr=i*i;
+    i++;
+    }
+    
+    
+    if(sqr>num)
+    {
+        System.out.println("Not perfect Number");
+    }
+    }
+}
+
+*/
