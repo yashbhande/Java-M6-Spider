72f6818b31a5259598606165396f8a6b8d155cfd
Yash Bhande <138775827+yashbhande@users.noreply.github.com>
2024-04-08 00:53:15 +0530

    Create FactorsOfNumber.java

---
+++ Recursion/FactorsOfNumber.java
@@ -0,0 +1,31 @@
+// Factors of Number 
+// Num: 12 factors ------> 1,2,3,4,6,12
+// Integer that divides the number exactly, leaving no remainder.
+
+
+class FactorsOfNumber {
+    static int num = 50; // Number to find factor of
+    static int i = 1;    // Starting divisor
+    
+    public static void main(String[] args) {
+        factors(); // Call the method to find factors
+    }
+    
+    public static void factors() {
+        // Check if 'i' is a factor of 'num' and print it if it is
+        if (num % i == 0) {
+            System.out.println(i);
+        }
+        
+        i++; // Increment divisor for the next iteration
+        
+        // Base case: If 'i' exceeds 'num', stop recursion
+        if (i > num) {
+            return;
+        }
+        
+        factors(); // Recursive call to continue finding factors
+    }
+}
+
+
