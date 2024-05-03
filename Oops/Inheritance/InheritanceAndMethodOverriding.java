a31b4a060da6c4cfa34d4310106a23c661917fe6
Yash Bhande <138775827+yashbhande@users.noreply.github.com>
2024-05-03 23:58:05 +0530

    Update InheritanceAndMethodOverriding.java

--- Oops/Inheritance/InheritanceAndMethodOverriding.java
+++ Oops/Inheritance/InheritanceAndMethodOverriding.java
@@ -1,54 +1,40 @@
-class Dog extends Animal        //Sub Class --------> Parent is Animal, Cat
+/******* Output *******
+Hello from Cat Class
+Hello from Animal Class
+Hello from Dog Class
+Hello from Main Class
+Barking...
+Eating....
+Mewww...
+*/
+
+class Cat // Represents a cat (Parent class)
 {
-    static {
-        System.out.println("Hello from Dog Class");
-    }
-    void bark()
-    {
-        System.out.println("Barking...");
-    }
+    static { System.out.println("Hello from Cat Class"); } // Static block for Cat class initialization
+    void cat() { System.out.println("Mewww..."); } // Method for cat sound
 }
 
-
-
-class Cat       // Parent Class
+class Animal extends Cat // Represents an animal (Child class inheriting from Cat)
 {
-    static {
-        System.out.println("Hello from CAt Class");
-    }
-    void cat()
-    {
-        System.out.println("Mewww...");
-    }
+    static { System.out.println("Hello from Animal Class"); } // Static block for Animal class initialization
+    void eat() { System.out.println("Eating...."); } // Method to eat
 }
 
-
-class Animal extends Cat        //Sub Class --------> Parent is Cat
+class Dog extends Animal  // Represents a dog (Grandchild class inheriting from Animal)
 {
-    static {
-        System.out.println("Hello from Animal Class");
-    }
-    void eat()
-    {
-        System.out.println("Eating....");
-    }
+    static { System.out.println("Hello from Dog Class"); } // Static block for Dog class initialization
+    void bark() { System.out.println("Barking..."); } // Method to bark
 }
 
-
-
-public class  InheritanceAndMethodOverriding extends Dog    // --------> Parent is Dog, Animal, Cat
+public class InheritanceAndMethodOverriding extends Dog // Main class extending Dog class
 {
-    static {
-        System.out.println("Hello from Main Class");
-    }
-    public static void main(String[] args)
+    static { System.out.println("Hello from Main Class"); } // Static block for Main class initialization
+
+    public static void main(String[] args) 
     {
-        // Dog d=new Dog();
-        // d.bark();
-        
-        InheritanceAndMethodOverriding h=new InheritanceAndMethodOverriding();
-        h.bark();
-        h.eat();
-        h.cat();
+        InheritanceAndMethodOverriding h = new InheritanceAndMethodOverriding(); // Creating an object of InheritanceAndMethodOverriding
+        h.bark(); // Calling bark method from Dog class
+        h.eat(); // Calling eat method from Animal class
+        h.cat(); // Calling cat method from Cat class
     }
 }
