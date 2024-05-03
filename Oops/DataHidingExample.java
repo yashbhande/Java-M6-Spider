97fe777a4471bf9d57a6c48a539a1d98d8505ad8
Yash Bhande <138775827+yashbhande@users.noreply.github.com>
2024-05-04 01:20:45 +0530

    Create DataHidingExample.java
    This Java program demonstrates the concept of data hiding using a Rectangle class. Data hiding is achieved by making the length and breadth variables private, which prevents direct access to these variables from outside the class. Getter and setter methods are used to access and modify these private variables, allowing controlled access to the data. The main class, DataHidingExample, creates an object of the Rectangle class, sets its dimensions using setter methods, and then retrieves and displays the dimensions and area of the rectangle using getter methods and a method to calculate the area. This program illustrates the encapsulation principle in object-oriented programming, enhancing data security and abstraction.

---
+++ Oops/DataHidingExample.java
@@ -0,0 +1,29 @@
+class Rectangle  // Represents a rectangle and demonstrates data hiding
+{
+    private double length;  // Private variable for hiding data
+    private double breadth; // Private variable for hiding data
+    
+    public double getLength()   { return length;  }     // Getter method to extract length data
+    public double getBreadth()  { return breadth; }    // Getter method to extract breadth data
+    
+    public void setLength(double a) { length = a; }     // Setter method to set length data
+    public void setBreadth(double b) { breadth = b; }   // Setter method to set breadth data
+    
+    public double Area() { return length * breadth; }   // Method to calculate and return area
+}
+
+
+public class DataHidingExample  // Main class demonstrating data hiding
+{
+    public static void main(String[] args)
+    {
+        Rectangle rect = new Rectangle();  // Creating an object of Rectangle class
+        rect.setLength(10);   // Setting length data using setLength
+        rect.setBreadth(20);  // Setting breadth data using setBreadth
+        
+        System.out.println("Length is : " + rect.getLength());    // Extracting length data using getLength
+        System.out.println("Breadth is : " + rect.getBreadth());  // Extracting breadth data using getBreadth
+        
+        System.out.println("Area : " + rect.Area());  // Calculating and printing the area of the rectangle
+    }
+}
