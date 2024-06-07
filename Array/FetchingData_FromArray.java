c85f96791adbf39e441d5538a15731a3fb3b3749
Yash Bhande <138775827+yashbhande@users.noreply.github.com>
2024-06-07 14:44:18 +0530

    Create FetchingData_FromArray.java

---
+++ Array/FetchingData_FromArray.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+class FetchingData_FromArray {
+    public static void main(String[] args) 
+    {
+        String names[]={"Yash","Nish","Amrit","Bca"};
+        System.out.println("Name on position 2 :"+names[2]);
+        
+        for(int i=0; i<names.length; i++)
+        {
+            System.out.println(names[i]);
+        }
+    }
+}
