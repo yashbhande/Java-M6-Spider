1d157377d0015f3677d03110f14b1c57009b5d40
Yash Bhande <138775827+yashbhande@users.noreply.github.com>
2024-06-07 15:35:49 +0530

    Create Fetch_NegativeNumbersFrom_Array.java

---
+++ Array/Fetch_NegativeNumbersFrom_Array.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+class Fetch_NegativeNumbersFrom_Array 
+{
+    public static void main(String[] args) 
+    {
+        int a[] = {1,2,3,-4,3,-5,6,-8};
+        System.out.print("Negative Numbers are :");
+        for(int i=0; i<a.length; i++)
+        {   
+            if(a[i]<0)
+            {
+            System.out.print( a[i]+" ");
+            }
+        }
+    }
+}
