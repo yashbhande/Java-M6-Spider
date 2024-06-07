e7df99bcf93b3186f65224b57a389f11f26d7743
Yash Bhande <138775827+yashbhande@users.noreply.github.com>
2024-06-07 14:05:31 +0530

    Update Find_ArrayLength_UsingLogic.java

--- Array/Find_ArrayLength_UsingLogic.java
+++ Array/Find_ArrayLength_UsingLogic.java
@@ -2,17 +2,17 @@ import java.util.*; // Importing the utility package which includes the Arrays c
 
 class Find_ArrayLength_UsingLogic {
     public static void main(String[] args) {
-        int a[][] = {{10, 20}, {30, 40, 50}, {60, 70}}; // Initializing a 2D array
-        int length = 0; // Variable to keep track of the array length
-        // System.out.println("2D Array : " + Arrays.deepToString(a));
+        int a[] = {10,20,3,40,50,60,70}; 
+        int length = 0; // To keep Array Length
 
-        // Infinite loop to determine the length of the array
-        for (;;) {
-            try {
-                // Access the sub-array and increase length
-                int[] temp = a[length++]; // Increment length after accessing the sub-array
-            } catch (Exception e) {
-                // Break the loop if an exception occurs (e.g., ArrayIndexOutOfBoundsException)
+        for (;;) //infinite loop
+        {
+            try 
+            {
+                System.out.println(a[length++]);
+            } catch (Exception e) 
+            {
+               // System.out.println(e.getMessage());
                 break;
             }
         }
