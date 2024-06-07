17dc8772a1dc4049aa63205b98f68923ab6e0e6c
Yash Bhande <138775827+yashbhande@users.noreply.github.com>
2024-06-07 14:10:55 +0530

    Create FindArrayLength_Via_ForEach.java

---
+++ Array/FindArrayLength_Via_ForEach.java
@@ -0,0 +1,16 @@
+import java.util.*; // Importing the utility package which includes the Arrays class
+
+class FindArrayLength_Via_ForEach
+{
+    public static void main(String[] args)
+    {
+        int arr[]={10,20,30,40,50,60};
+        int length=0;
+        
+        for(int i : arr)
+        {
+            length++;
+        }
+        System.out.println("The length of Array is: "+length);
+    }
+}
