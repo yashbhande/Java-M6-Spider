5b1d33f49ac55758b6f897bdbe2dac99524b8630
Yash Bhande <138775827+yashbhande@users.noreply.github.com>
2024-06-11 22:57:16 +0530

    Create AssciValueOfChar_FromArray.java

---
+++ Array/AssciValueOfChar_FromArray.java
@@ -0,0 +1,23 @@
+import java.util.*; // Importing the necessary classes from the java.util package
+
+class AssciValueOfChar_FromArray 
+{
+    public static void main(String[] args) 
+  {  
+        char ch[] = {'A', 'D', 'Z', '@'};
+        System.out.println(Arrays.toString(ch)); //Print original array
+        
+        for (char yash : ch) // Loop through each character in the array
+        {
+            int asciiValue = (int) yash;// Cast the character to an int to get the ASCII value            
+            System.out.println("ASCII Value of " + yash + " is " + asciiValue); // Print the character and its corresponding ASCII value
+        }
+    }
+}
+
+
+// [A, D, Z, @]
+// Assci Value of is 65
+// Assci Value of is 68
+// Assci Value of is 90
+// Assci Value of is 64
