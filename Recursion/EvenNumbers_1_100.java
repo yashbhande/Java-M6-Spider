0d2c0645e38db19a32f28d79d490edb9a76dff38
Yash Bhande <138775827+yashbhande@users.noreply.github.com>
2024-06-18 20:23:05 +0530

    Update EvenNumbers_1_100.java

--- Recursion/EvenNumbers_1_100.java
+++ Recursion/EvenNumbers_1_100.java
@@ -1,25 +1,26 @@
 // This program prints first 100 even numbers recursively
 // 2 4 6 8 10 12..................100
 
-class EvenNumbers_1_100 {
+class EvenNumbers_1_100
+{
     static int number = 1; // Initialize the number variable to start checking from 1
 
-    public static void main(String[] args) {
+    public static void main(String[] args) 
+    {
         evenNumbers(); // Calling method
     }
-
     
-    public static void evenNumbers() {
-        // Check if the number is even and print
-        if (number % 2 == 0) {
+    public static void evenNumbers() //callled method
+    {
+        if (number % 2 == 0) // Check if the number is even and print
+        {
             System.out.print(number + " "); // Print the even number
         }
         
-        // Base case: If number reaches 100, stop recursion
-        if (number >= 100) {
+        if (number >= 100) // Base case: If number reaches 100, stop recursion
+        {
             return;
         }
-
         number++; // Increment number for next iteration
         evenNumbers(); // Recursive call 
     }
