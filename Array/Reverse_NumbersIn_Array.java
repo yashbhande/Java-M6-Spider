a717ceb37722bcf152c2b0a2ff89d427c9a76593
Yash Bhande <138775827+yashbhande@users.noreply.github.com>
2024-06-18 19:05:04 +0530

    Update Reverse_NumbersIn_Array.java

--- Array/Reverse_NumbersIn_Array.java
+++ Array/Reverse_NumbersIn_Array.java
@@ -1,11 +1,13 @@
-import java.util.*;
+// a[] = {1,2,3,-4,3,-5,6,8};
+//Reversed Numbers are :8 6 -5 3 -4 3 2 1 
 
+import java.util.*;
 class Reverse_NumbersIn_Array
 {
     public static void main(String[] args) 
     {
         int a[] = {1,2,3,-4,3,-5,6,8};
-        System.out.print("Negative Numbers are :");
+        System.out.print("Reversed Numbers are :");
         for(int i=a.length-1; i>=0; i--)
         {   
             System.out.print( a[i]+" ");
@@ -13,4 +15,3 @@ class Reverse_NumbersIn_Array
     }
 }
 
-//Negative Numbers are :8 6 -5 3 -4 3 2 1 
