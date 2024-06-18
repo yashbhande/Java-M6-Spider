c13e127b8f518456fed23190a6ff59e8e7c56701
Yash Bhande <138775827+yashbhande@users.noreply.github.com>
2024-06-18 19:32:45 +0530

    Update SecondLargestElement.java

--- Array/SecondLargestElement.java
+++ Array/SecondLargestElement.java
@@ -1,47 +1,42 @@
 //Wap find second largest element in the array
 //1. Sort Array
 //2. Find 2nd Largest Element in array (length-2 index)
-// Input: [2, 22, 33, 44, 55]
-// Second largest elemnt is : 44
+// Original Array: [5, 10, 22, -5]
+// Sorted Array: [-5, 5, 10, 22]
+// 2nd Largest Element: 10
 
 import java.util.*;
 class SecondLargestElement
 {
 
-static{
-	System.out.println("This Program is to Fetch the 2nd max Element in Array.");
-}
-public static void main(String[] args) 
-{
-		
-	Scanner sc=new Scanner(System.in);
-	System.out.println("Enter size");
-	int size=sc.nextInt();
-
-	System.out.println("Enter charactes");
-	int[] a=new int[size];
-
-	for(int i=0;i<size;i++)
-	{
-		a[i]=sc.nextInt();
-	}
+static Scanner sc=new Scanner(System.in);
+  public static void main(String[] args) 
+  {
+    System.out.println("How many elements you want to store in Array ???");
+    int arraySize=sc.nextInt();
+    int a[]= new int[arraySize];
+    int pos=0;
+    for(int temp:a)
+    {
+        System.out.println("Enter Element:");
+        a[pos++]=sc.nextInt();
+    }
+	System.out.println("Original Array: "+Arrays.toString(a));	
 
 	int temp=0;
-	for(int i=0; i<size; i++)
+	for(int i=0; i<a.length; i++)
 	{
-		for(int j=i+1; j<size;j++)
-		{
-			if(a[i]>a[j])
-			{
-				temp=a[i];
-				a[i]=a[j];
-				a[j]=temp;
-			}
-		}
+	    for(int j=i+1; j<a.length; j++)
+	    {
+	        if(a[i]>a[j])
+	        {
+	            temp=a[i];
+	            a[i]=a[j];
+	            a[j]=temp;
+	        }
+	    }
 	}
-	int temp1=a[a.length-2]; //(length - 2 Index) to find 2nd highest element in sorted array
-
-	System.out.println(Arrays.toString(a));	//print sorted Array
-	System.out.println("Second largest elemnt is : "+temp1); //print 2nd highest elemnt
-}
+	System.out.println("Sorted Array: "+Arrays.toString(a)); //Print Original Sorted Array
+	System.out.println("2nd Largest Element: "+a[a.length-2]);  //Print 2nd Largest Element
+  }
 }
