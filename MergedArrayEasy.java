e9b565b6ca5cc1d40384f6d1feb180d28363630c
Yash Bhande <138775827+yashbhande@users.noreply.github.com>
2024-06-18 18:35:17 +0530

    Update MergedArrayEasy.java
    New Easy method to convert to array

--- MergedArrayEasy.java
+++ MergedArrayEasy.java
@@ -1,24 +1,24 @@
+//Sample 
 // a:[1,2,3]
 // b:[4,5,6]
 // Output: [1, 2, 3, 4, 5, 6]
+
 import java.util.*;
 class MergedArrayEasy
 {
-    public static void main(String[] args)
+   public static void main(String[] args) 
     {
-	int a[]={1,2,3};
-	int b[] ={4,5,6};
-	int c[]= new int[a.length+ b.length]; //new Array to store merged elements
-	int pos=0;  //to itterate the array C index
+        int a[] = {1, 2, 3, 4};
+        int b[] = {5, 6, 7,5,4,2,1};
 	    
-	for (int i = 0; i < a.length; i++) 
-	{
-            c[pos++] = a[i];//storing Array "a" elemnets in Array "C"
-      	}
-	for (int i = 0; i < b.length; i++) 
-	{
-            c[pos++] = b[i];//storing Array "b" elemnets in Array "C"
-        }
-	System.out.println(Arrays.toString(c));
-     }
+        int merge[]= new int [a.length+b.length]; //giving size to new Array to store the elemnets in new Array
+        int pos=0; //using as index for new array to store elemensts on new index
+        
+        for(int i=0; i<(a.length+b.length); i++) //itterate though all elements of array a & b
+        {
+           if(i<a.length) merge[pos++]= a[i];	//store array 'a' elements in new array
+           if(i<b.length) merge[pos++]= b[i];	//store array 'b' elements in new array
+        }  
+        System.out.print("Merrged Array: "+Arrays.toString(merge));
+    }
 }
