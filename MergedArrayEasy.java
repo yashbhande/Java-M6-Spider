a3e91eb012ab65ff78a04ffec05abeda9a5a0f06
Yash Bhande <138775827+yashbhande@users.noreply.github.com>
2024-06-18 18:20:09 +0530

    Update MergedArrayEasy.java

--- MergedArrayEasy.java
+++ MergedArrayEasy.java
@@ -4,22 +4,21 @@
 import java.util.*;
 class MergedArrayEasy
 {
-	public static void main(String[] args)
+    public static void main(String[] args)
+    {
+	int a[]={1,2,3};
+	int b[] ={4,5,6};
+	int c[]= new int[a.length+ b.length]; //new Array to store merged elements
+	int pos=0;  //to itterate the array C index
+	    
+	for (int i = 0; i < a.length; i++) 
 	{
-	    int a[]={1,2,3};
-	    int b[] ={4,5,6};
-	    int c[]= new int[a.length+ b.length]; //new Array to store merged elements
-	    int pos=0;  //to itterate the array C index
-	    
-	    for (int i = 0; i < a.length; i++) 
-	    {
-          c[pos++] = a[i];//storing Array "a" elemnets in Array "C"
-      }
-        for (int i = 0; i < b.length; i++) 
-	    {
-          c[pos++] = b[i];//storing Array "b" elemnets in Array "C"
-      }
-	    System.out.println(Arrays.toString(c));
-	    
-	}
+            c[pos++] = a[i];//storing Array "a" elemnets in Array "C"
+      	}
+	for (int i = 0; i < b.length; i++) 
+	{
+            c[pos++] = b[i];//storing Array "b" elemnets in Array "C"
+        }
+	System.out.println(Arrays.toString(c));
+     }
 }
